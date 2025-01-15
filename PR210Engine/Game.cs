583a8a3145e9075d079a81b2dcf5dbf161c15a80
Max Heinze <maxheinze@googlemail.com>
2025-01-15 17:08:23 +0100

    Game, overrides from OpenTK

---
+++ PR210Engine/Game.cs
@@ -0,0 +1,43 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Graphics.ES11;
+
+namespace PR210Engine
+{
+    public class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(GameWindowSettings.Default,
+            new NativeWindowSettings() { Size = (width, height), Title = title })
+        {
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            SwapBuffers();
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.ClearColor(0.5f,0.5f,0.5f,0.5f);
+        }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs args)
+        {
+            base.OnFramebufferResize(args);
+            GL.Viewport(0,0,args.Width, args.Height);
+        }
+    }
+}
